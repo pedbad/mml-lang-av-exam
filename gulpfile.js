4bcaffd2de67d0e482fe1b46676746fe1d97b737
pedbad <pb357@cam.ac.uk>
2018-11-09 15:40:17 +0000

    reafctored code and added mediaelement.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,8 @@ var orderedStreams = require('ordered-read-streams');
 var SOURCEPATH = {
   sassSource : 'src/scss/*.scss',
   htmlSource : 'src/*.html',
-  jsSource : 'src/js/**'
+  jsSource : 'src/js/**',
+  imgSource: 'src/img/**'
 }
 
 var APPPATH = {
@@ -21,7 +22,8 @@ var APPPATH = {
   css : 'app/css',
   js : 'app/js',
   webfonts: 'app/webfonts',
-  mediaelement: 'app/mediaelement'
+  mediaelement: 'app/mediaelement',
+  img : 'app/img'
 
 }
 
@@ -35,12 +37,36 @@ gulp.task('clean-scripts', function(){
     .pipe(clean());
 });
 
-gulp.task('scripts', ['clean-scripts'], function(){
+gulp.task('clean-images', function(){
+  return gulp.src(APPPATH.img + '/*.{jpg,png,svg}', {read: false, force: true})
+    .pipe(clean());
+});
+
+
+// Move JS Files to src/js
+gulp.task('scripts', ['clean-scripts'], function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js',
+     'node_modules/jquery/dist/jquery.min.js',
+     'node_modules/popper.js/dist/umd/popper.min.js',
+     'node_modules/bootstrap-select-v4/dist/js/bootstrap-select.min.js',
+     SOURCEPATH.jsSource + 'main.js'])
+        .pipe(gulp.dest(APPPATH.js))
+        .pipe(browserSync.stream());
+});
+
+
+
+/*
+gulp.task('scripts',  ['js', 'clean-scripts'], function(){
   gulp.src(SOURCEPATH.jsSource)
     .pipe(concat('main.js'))
     .pipe(browserify())
-    .pipe(gulp.dest(APPPATH.js));
+    .pipe(gulp.dest(APPPATH.js))
+    .pipe(browserSync.stream());
 });
+
+*/
+
 
 gulp.task('copy', ['clean-html'], function(){
   gulp.src(SOURCEPATH.htmlSource)
@@ -49,7 +75,9 @@ gulp.task('copy', ['clean-html'], function(){
 
 
 
+
 gulp.task('sass', function(){
+  var mediaelementCSS = gulp.src('./node_modules/mediaelement/build/mediaelementplayer.css');
   var fontawesomeCSS = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/all.css');
   var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
   var sassFiles;
@@ -58,7 +86,7 @@ gulp.task('sass', function(){
     .pipe(autoprefixer())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     //.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    return orderedStreams([fontawesomeCSS, bootstrapCSS, sassFiles])
+    return orderedStreams([mediaelementCSS, fontawesomeCSS, bootstrapCSS, sassFiles])
       .pipe(concat('app.css'))
       .pipe(gulp.dest(APPPATH.css));
 });
@@ -68,6 +96,12 @@ gulp.task('sass', function(){
 gulp.task('move-fonts', function() {
   return gulp.src(['./node_modules/@fortawesome/fontawesome-free/webfonts/*.{eot,svg,ttf,woff,woff2}'])
     .pipe(gulp.dest(APPPATH.webfonts))
+})
+
+// Move Images to app/img
+gulp.task('images', ['clean-images'], function() {
+  return gulp.src([SOURCEPATH.imgSource + '/*.{jpg,png,svg}'])
+    .pipe(gulp.dest(APPPATH.img))
 })
 
 // Move mediaelement to src/mediaelement
@@ -84,10 +118,11 @@ gulp.task('serve', ['sass'], function(){
   })
 });
 
-gulp.task('watch', ['serve', 'copy', 'scripts', 'move-fonts', 'mediaelement'], function(){
+gulp.task('watch', ['serve', 'copy', 'scripts', 'move-fonts', 'mediaelement', 'images'], function(){
   gulp.watch([SOURCEPATH.sassSource], ['sass']);
   gulp.watch([SOURCEPATH.htmlSource], ['copy']);
   gulp.watch([SOURCEPATH.jsSource], ['scripts']);
+  gulp.watch([SOURCEPATH.imgSource], ['images']);
 });
 
 gulp.task('default', ['watch'] );
--- src/js/scripts.js
+++
@@ -1,8 +0,0 @@
-global.jQuery = require('jquery');
-popper = require('popper.js');
-bootstrap = require('bootstrap');
-mustache = require('mustache');
-
-jQuery(document).ready(function(){
-  alert("hello");
-});
